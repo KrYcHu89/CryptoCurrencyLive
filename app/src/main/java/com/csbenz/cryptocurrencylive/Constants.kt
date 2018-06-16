fb3443914319847e22203e1c0fdd7cbc017d506f
Christopher Benz <christopher.benz24@gmail.com>
2018-06-17 01:12:42 +0200

    Corrected server adress

--- app/src/main/java/com/csbenz/cryptocurrencylive/Constants.kt
+++ app/src/main/java/com/csbenz/cryptocurrencylive/Constants.kt
@@ -4,12 +4,11 @@ object Constants {
 
     const val PAIR_NAME_BUNDLE_ID = "pair_name"
 
-    const val BITFINEX_PAIR_LIST_URL = "https://api.bitfinex.com/v1/symbolss"
+    const val BITFINEX_PAIR_LIST_URL = "https://api.bitfinex.com/v1/symbols"
 
     const val BITFINEX_PAIR_SUMMARY_URL_PREFIX = "https://api.bitfinex.com/v1/pubticker/"
 
     const val BITFINEX_WEBSOCKET_URL = "https://api.bitfinex.com/ws/2"
-
 
 
 }
